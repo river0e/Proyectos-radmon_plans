8c2aed6136ed2b53555cf696f4b067c55e531e95
river0e <mj.castro.rivero@gmail.com>
2025-08-13 13:35:07 +0200

    Filtros de busqueda y prueba de BBDD

--- random_plans/app.py
+++ random_plans/app.py
@@ -13,23 +13,35 @@ class Plan(db.Model):
     description = db.Column(db.String(200))
     location = db.Column(db.String(100))
     category = db.Column(db.String(50))
+    people = db.Column(db.Integer)       # Nuevo campo
+    duration = db.Column(db.Integer)     # Nuevo campo (minutos)
 
 @app.route('/')
 def index():
     search = request.args.get('search', '')
+    category = request.args.get('category', '')
+    people = request.args.get('people', '')
+    duration = request.args.get('duration', '')
+
+    query = Plan.query
 
     if search:
-        plans = Plan.query.filter(
+        query = query.filter(
             or_(
                 Plan.title.ilike(f'%{search}%'),
                 Plan.description.ilike(f'%{search}%'),
                 Plan.location.ilike(f'%{search}%')
             )
-        ).all()
-    else:
-        plans = Plan.query.all()
+        )
+    if category:
+        query = query.filter_by(category=category)
+    if people:
+        query = query.filter_by(people=int(people))
+    if duration:
+        query = query.filter_by(duration=int(duration))
 
-    return render_template('index.html', plans=plans, search=search)
+    plans = query.all()
+    return render_template('index.html', plans=plans, search=search, category=category, people=people, duration=duration)
 
 @app.route('/create', methods=['GET', 'POST'])
 def create_plan():
@@ -38,7 +50,17 @@ def create_plan():
         description = request.form['description']
         location = request.form['location']
         category = request.form['category']
-        new_plan = Plan(title=title, description=description, location=location, category=category)
+        people = int(request.form['people'])
+        duration = int(request.form['duration'])
+
+        new_plan = Plan(
+            title=title,
+            description=description,
+            location=location,
+            category=category,
+            people=people,
+            duration=duration
+        )
         db.session.add(new_plan)
         db.session.commit()
         return redirect('/')
@@ -46,29 +68,37 @@ def create_plan():
 
 @app.route('/random')
 def random_plan():
-    category = request.args.get('category')
+    category = request.args.get('category', '')
     search = request.args.get('search', '')
+    people = request.args.get('people', type=int)
+    duration = request.args.get('duration', type=int)
+
+    query = Plan.query
 
     if category:
-        plans = Plan.query.filter_by(category=category).all()
-    elif search:
-        plans = Plan.query.filter(
+        query = query.filter_by(category=category)
+    if search:
+        query = query.filter(
             or_(
                 Plan.title.ilike(f'%{search}%'),
                 Plan.description.ilike(f'%{search}%'),
                 Plan.location.ilike(f'%{search}%')
             )
-        ).all()
-    else:
-        plans = Plan.query.all()
-    
+        )
+    if people is not None:
+        query = query.filter_by(people=people)
+    if duration is not None:
+        query = query.filter_by(duration=duration)
+
+    plans = query.all()
+
     if not plans:
-        return render_template('random_plan.html', plan=None, category=category)
-    
+        return render_template('random_plan.html', plan=None, category=category, search=search, people=people, duration=duration)
+
     plan = random.choice(plans)
-    return render_template('random_plan.html', plan=plan, category=category)
+    return render_template('random_plan.html', plan=plan, category=category, search=search, people=people, duration=duration)
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
+        db.create_all() # 🔹 Crear nueva BBDD
     app.run(debug=True)
